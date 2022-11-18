cb32a004ba048b4ca83014ceca3e869a7d5b779b
Jorge <sandez.jorge@gmail.com>
2022-11-18 15:12:49 -0300

    fixes symbolic links!

--- start-controller.sh
+++ start-controller.sh
@@ -7,9 +7,17 @@ fi
 
 echo "Using $POXPATH as pox installation folder"
 cd src
-cp policies.json $POXPATH/ext/policies.json
-cp policies.py $POXPATH/ext/policies.py
-cp firewall.py $POXPATH/ext/firewall.py
+if [[ ! -f $POXPATH/ext/policies.json ]]; then
+  ln -s $PWD/policies.json $POXPATH/ext/policies.json
+fi
+
+if [[ ! -f $POXPATH/ext/policies.py ]]; then
+  ln -s $PWD/policies.py $POXPATH/ext/policies.py
+fi
+
+if [[ ! -f $POXPATH/pox/samples/firewall.py ]]; then
+  ln -s $PWD/firewall.py $POXPATH/pox/samples/firewall.py
+fi
 
 echo "Running $POXPATH/pox.py --verbose firewall"
-$POXPATH/pox.py --verbose firewall
+$POXPATH/pox.py --verbose samples.firewall
